fd6618cde719959f5023ae951da562897a368128
JPazos06 <66483938+JPazos06@users.noreply.github.com>
2023-04-11 18:14:51 -0500

    Base de Datos
    Integración de Base de Datos al proyecto

---
+++ Models/DB/EstablecimientoDeSalud.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BBT_Plataforma_Establecimientos_De_Salud.Models.DB;
+
+public partial class EstablecimientoDeSalud
+{
+    public int Id { get; set; }
+
+    public int CodigoRenaes { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Ciudad { get; set; } = null!;
+
+    public string Red { get; set; } = null!;
+
+    public string Direccion { get; set; } = null!;
+
+    public decimal Longitud { get; set; }
+
+    public decimal Latitud { get; set; }
+
+    public string? Descripcion { get; set; }
+
+    public string? Imagen { get; set; }
+
+    public virtual ICollection<Busquedum> Busqueda { get; set; } = new List<Busquedum>();
+
+    public virtual ICollection<Valoracion> Valoracions { get; set; } = new List<Valoracion>();
+}
---
+++ Models/DB/Valoracion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BBT_Plataforma_Establecimientos_De_Salud.Models.DB;
+
+public partial class Valoracion
+{
+    public int Id { get; set; }
+
+    public int EstablecimientoId { get; set; }
+
+    public int UsuarioId { get; set; }
+
+    public string? Comentario { get; set; }
+
+    public int? Valoracion1 { get; set; }
+
+    public virtual EstablecimientoDeSalud Establecimiento { get; set; } = null!;
+
+    public virtual Usuario Usuario { get; set; } = null!;
+}
